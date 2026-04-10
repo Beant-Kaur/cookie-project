4dff0161cbd6eeb90631bb45710f5ceddf9bb772
Beant-Kaur <beant8410@gmail.com>
2026-04-10 18:28:57 +0530

    Initial setup with server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const cookieParser = require("cookieParser");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () =>{
+    console.lod("Server running on port 3000");
+});
